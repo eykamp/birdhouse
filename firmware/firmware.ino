646696e70497f68b1fa24d314145a3702f1d669c
Unknown <chris@eykamp.com>
2017-11-21 12:30:10 -0800

    More informative scan display

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -541,7 +541,7 @@ void printScanResult() {
   Serial.printf("%d network(s) found\n", networksFound);
 
   for (int i = 0; i < networksFound; i++) {
-    Serial.printf("%d: %s, Ch:%d (%ddBm) %s\n", i + 1, WiFi.SSID(i).c_str(), WiFi.channel(i), WiFi.RSSI(i), WiFi.encryptionType(i) == ENC_TYPE_NONE ? "open" : "");
+    Serial.printf("%d: %s <<%s>>, Ch:%d (%ddBm) %s\n", i + 1, WiFi.SSID(i) == "" ? "[Hidden network]" : WiFi.SSID(i).c_str(), WiFi.BSSIDstr(i).c_str(), WiFi.channel(i), WiFi.RSSI(i), WiFi.encryptionType(i) == ENC_TYPE_NONE ? "open" : "");
   }
 
   String json = "{\"visibleNetworks\":\"["; 
