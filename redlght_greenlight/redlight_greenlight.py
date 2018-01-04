e8caaab7ec39926bf9f93c1ef55503ee80bf783b
Chris Eykamp <chris@eykamp.com>
2018-01-04 16:37:20 -0500

    Server side enhancements to allow remote firmware updates

--- redlght_greenlight/redlight_greenlight.py
+++ redlght_greenlight/redlight_greenlight.py
@@ -3,6 +3,9 @@ import json
 import web              # sudo pip3 install git+https://github.com/webpy/webpy#egg=web.py
 import googlemaps       # sudo pip install googlemaps
 import geopy.distance   # sudo pip install geopy
+import re
+import os
+import base64
 
 # pip install git+git://github.com/eykamp/thingsboard_api_tools.git --upgrade
 # sudo pip install git+git://github.com/eykamp/thingsboard_api_tools.git --upgrade
@@ -16,7 +19,8 @@ gmaps = googlemaps.Client(key=google_geolocation_key)
 
 urls = (
     '/', 'set_led_color',
-    '/hotspots/', 'handle_hotspots'
+    '/hotspots/', 'handle_hotspots',
+    '/update/', 'handle_update'
 )
 
 app = web.application(urls, globals())
@@ -78,6 +82,33 @@ class handle_hotspots:
             print("Error sending location telemetry!")
             return
 
+class handle_update:
+    def GET(self):
+        current_version = web.ctx.env.get('HTTP_X_ESP8266_VERSION')
+
+        v = re.search("(\d+)\.(\d+)", current_version)
+        major = int(v.group(1))
+        minor = int(v.group(2))
+
+        next_version = '/tmp/firmware_' + str(major) + '.' + str(minor + 1) + '.bin'
+        print("Looking for version", next_version)
+
+        if os.path.exists(next_version):
+            # domain = re.sub(r':[0-9]+$', '', web.ctx.homedomain)
+            # raise web.SeeOther(domain + '/firmware_images/firmware_1.24.bin')       # Handled by Apache
+
+            file = open(next_version, 'rb')
+            bin_image = file.read()
+            byte_count = str(len(bin_image))
+
+            print("Sending update (" + byte_count + " bytes)")
+
+            web.header('Content-type','application/octet-stream')
+            web.header('Content-transfer-encoding','base64') 
+            web.header('Content-length', byte_count)
+            return bin_image
+
+        raise web.NotModified()
 
 class set_led_color:        
     def POST(self):
