1ad75ed8e48a043797c97131bd77aea668108e75
Chris Eykamp <chris@eykamp.com>
2018-01-07 20:04:16 -0500

    Better message

--- redlght_greenlight/redlight_greenlight.py
+++ redlght_greenlight/redlight_greenlight.py
@@ -135,7 +135,7 @@ class set_led_color:
         web.header('Content-Type', 'application/json')
         data = { "nonce": color }
 
-        print(json.dumps(data))
+        print("JSON dump:", json.dumps(data))
 
         return json.dumps(data)
 
