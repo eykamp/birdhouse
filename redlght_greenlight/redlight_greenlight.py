09f73386204f50b5aa2170534d1fe67ee5d1230c
Chris Eykamp <chris@eykamp.com>
2018-01-07 19:50:34 -0500

    More verbose messages

--- redlght_greenlight/redlight_greenlight.py
+++ redlght_greenlight/redlight_greenlight.py
@@ -31,7 +31,7 @@ class handle_hotspots:
             data = web.data()
             decoded = data.decode(data_encoding)
             incoming_data = json.loads(decoded)
-            print(incoming_data)
+            # print(incoming_data)
 
             known_lat = incoming_data["latitude"]
             known_lng = incoming_data["longitude"]
@@ -52,7 +52,7 @@ class handle_hotspots:
 
         results = gmaps.geolocate(wifi_access_points=incoming_data["hotspots"])
 
-        print("Geocoding results:", results)
+        print("Geocoding results for " + device_token + ":", results)
 
         if "error" in results:
             print("Received error from Google API!")
@@ -114,11 +114,12 @@ class set_led_color:
     def POST(self):
         # Decode request data
 
-        print("Received data: ", web.data().decode(data_encoding))
         incoming_data = json.loads(str(web.data().decode(data_encoding)))
 
         temperature = incoming_data["temperature"]
         device_id = incoming_data["device_id"]
+
+        print("Received data for " + device_id + ": ", web.data().decode(data_encoding))
 
         if float(temperature) < 50:
             color = 'GREEN'
