ca2228c4bac4cd0bbeb845a81964d239653e2230
Unknown <chris@eykamp.com>
2018-02-06 15:52:21 -0800

    Change typedef

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -33,7 +33,7 @@
 #define S16 int16_t
 #define U16 uint16_t
 #define S32 int32_t 
-#define U32 uint32_t            // unsigned long
+#define U32 uint32            // unsigned long
 #define F32 float
 #define F64 double
 
