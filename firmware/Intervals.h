b829f70a995981011c67d712c5850e2955d24a7e
Unknown <chris@eykamp.com>
2018-03-04 21:19:09 -0800

    Break these out for reuse

---
+++ firmware/Intervals.h
@@ -0,0 +1,8 @@
+// Interval definitions
+#define MILLIS 1
+#define SECONDS 1000
+#define MINUTES 60 * SECONDS
+#define HOURS 60 * MINUTES
+#define DAYS 24 * HOURS
+#define MILLIS_TO_MICROS 1000
+#define SECONDS_TO_MICROS SECONDS * MILLIS_TO_MICROS
---
+++ firmware/Types.h
@@ -0,0 +1,8 @@
+#define U8  uint8_t
+#define S8  int8_t
+#define S16 int16_t
+#define U16 uint16_t
+#define S32 int32_t 
+#define U32 uint32            // unsigned long
+#define F32 float
+#define F64 double
