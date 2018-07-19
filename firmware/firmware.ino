678362cb2ff24d780e511ce684ca2b85105c6e60
Unknown <chris@eykamp.com>
2018-07-18 18:58:06 -0700

    Remove comments

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -39,22 +39,6 @@
 #define PRESSURE_UNIT    BME280::PresUnit_hPa
 
 
-
-
-// Verified
-// #define D0    16
-// #define D1      5   // I2C Bus SCL (clock)
-// #define D2      4   // I2C Bus SDA (data)
-// #define D3      0
-// #define D4      2   // Also blinks on-board blue LED, but with inverted logic
-// #define D5    14   // SPI Bus SCK (clock)
-// #define D6    12   // SPI Bus MISO 
-// #define D7    13   // SPI Bus MOSI
-// #define D8    15   // SPI Bus SS (CS)
-// #define D9      3   // RX0 (Serial console)
-// #define D10    1   // TX0 (Serial console)
-
-// Pin layout
 #define BME_SCL D5  // SPI (Serial Clock)
 #define BME_SDA D6  // SDA (Serial Data) 
 
