1f8393103f811d43d0bc1a3fc4faf7b48cffc769
Unknown <chris@eykamp.com>
2018-10-31 18:54:31 -0700

    Create update_customer function

--- management/birdhouse_utils.py
+++ management/birdhouse_utils.py
@@ -47,6 +47,18 @@ def one_line_address(cust_info):
     return cust_info["address"] + ", " + ((cust_info["address2"] + ", ") if cust_info["address2"] is not None and cust_info["address2"] != "" else "") + cust_info["city"] + ", " + cust_info["state"] 
 
 
+def update_customer(tbapi, cust_info):
+    tbapi.update_customer(cust_info["cust_id"], cust_info["name"], cust_info["address"], cust_info["address2"], cust_info["city"], cust_info["state"], cust_info["zip"], cust_info["country"], cust_info["email"], cust_info["phone"])
+
+    server_attributes = {
+        "latitude": cust_info["lat"],
+        "longitude": cust_info["lon"],
+        "address": one_line_address(cust_info)
+    }
+
+    tbapi.set_server_attributes(device_id, cust_info["device_id"])
+
+
 def update_customer_data(cust_info):    
     location = geocode(cust_info["address"], cust_info["address2"], cust_info["city"], cust_info["state"], cust_info["zip"], cust_info["country"])
 
