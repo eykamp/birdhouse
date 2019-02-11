3bcf463c2f3a11c0ce51747b81fe188a4c72b136
Unknown <chris@eykamp.com>
2019-02-11 00:00:27 -0800

    Pass IPs

--- management/migration_tools.py
+++ management/migration_tools.py
@@ -30,7 +30,7 @@ column_list = "entity_type, entity_id, key, ts, bool_v, str_v, long_v, dbl_v"
 def main():
     # report_device_info([1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 15, 17, 18, 20, 21, 24, 25, 27, 28, 29, 30, 33, 35, 36, 37, 38, 39, 41, 42, 44, 46, 47, 90, 100, 101, 102, 114, 116, 117, 118, 119, 120])
     # report_device_info([8,14,16,22,23,26,31,32,34,40,43,45])
-    find_unmigrated_devices()   # This still isn't right -- devices that are not online are marked as not ready for migration... maybe other problems
+    find_unmigrated_devices(old_server_ip, new_server_ip)   # This still isn't right -- devices that are not online are marked as not ready for migration... maybe other problems
     exit()
 
 
@@ -69,12 +69,12 @@ def main():
     new_client.close()
 
 
-def find_unmigrated_devices():
-    tbapi_old = TbApi(birdhouse_utils.make_mothership_url(old_server_ip), thingsboard_username, thingsboard_password)
-    tbapi_new = TbApi(birdhouse_utils.make_mothership_url(new_server_ip), thingsboard_username, thingsboard_password)
+def find_unmigrated_devices(old_ip, new_ip):
+    tbapi_old = TbApi(birdhouse_utils.make_mothership_url(old_ip), thingsboard_username, thingsboard_password)
+    tbapi_new = TbApi(birdhouse_utils.make_mothership_url(new_ip), thingsboard_username, thingsboard_password)
 
-    old_client = create_client(old_server_ip)
-    new_client = create_client(new_server_ip)
+    old_client = create_client(old_ip)
+    new_client = create_client(new_ip)
 
     devices_on_old = tbapi_old.get_all_devices()
     devices_on_new = tbapi_new.get_all_devices()
